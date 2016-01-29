8beb508b15a6e85342fc88d77b432db949f65069
Andrew Lobanov <goodsteamtrade@live.ru>
2016-01-30 00:19:55 +0300

    Changed app version cause of released properties window

--- WoblaExplorer/Properties/AssemblyInfo.cs
+++ WoblaExplorer/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.5")]
-[assembly: AssemblyFileVersion("0.9.1.5")]
+[assembly: AssemblyVersion("0.9.2.5")]
+[assembly: AssemblyFileVersion("0.9.2.5")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
